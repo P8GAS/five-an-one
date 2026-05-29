aad374c13ba194060a2d035380ba8fdc97d50119
P8GAS <lucasmplays18@gmail.com>
2026-05-29 04:37:21 +0200

    build: endpoints for teams logo and roster

--- backend/routers/teams.py
+++ backend/routers/teams.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from nba_api.stats.static import teams
-from nba_api.stats.endpoints import teamyearbyyearstats, teamdetails
+from nba_api.stats.endpoints import teamdetails, commonteamroster
 from nba_api.library.http import NBAHTTP
 from services.cache import get_cache, set_cache
 import time
@@ -38,5 +38,43 @@ def get_info(team_id: int):
     time.sleep(0.6)
     info = teamdetails.TeamDetails(team_id=team_id)
     data = info.get_data_frames()[0].to_dict(orient="records")[0]
+    set_cache(cache_key, data)
+    return data
+
+@router.get("/{team_id}/logo")
+def get_logo(team_id: int):
+    cache_key = f"team_logo_{team_id}"
+    cached = get_cache(cache_key)
+    if cached:
+        return cached
+    time.sleep(0.6)
+    url = f"https://cdn.nba.com/logos/nba/{team_id}/primary/L/logo.svg"
+    set_cache(cache_key, {"url": url})
+    return {"url": url}
+
+@router.get("/{team_id}/roster")
+def get_roster(team_id: int):
+    cache_key = f"team_roster_{team_id}"
+    cached = get_cache(cache_key)
+    if cached:
+        return cached
+    time.sleep(0.6)
+    roster = commonteamroster.CommonTeamRoster(team_id=team_id)
+    roster = roster.get_data_frames()[0].to_dict(orient="records")
+    set_cache(cache_key, roster)
+    return roster
+
+@router.get("/{team_id}/card_info")
+def get_card_info(team_id: int):
+    cache_key = f"team_card_info_{team_id}"
+    cached = get_cache(cache_key)
+    if cached:
+        return cached
+    time.sleep(0.6)
+    data = {
+        "logo": get_logo(team_id),
+        "info": get_info(team_id),
+        "roster": get_roster(team_id)
+    }
     set_cache(cache_key, data)
     return data
